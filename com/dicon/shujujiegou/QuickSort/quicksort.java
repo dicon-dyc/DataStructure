36f720d1485f578e55b85bee64713a44c9d8f56c
dicon-dyc <1351109694@qq.com>
2021-07-07 21:46:46 +0800

    2021-7-7

---
+++ com/dicon/shujujiegou/QuickSort/quicksort.java
@@ -0,0 +1,55 @@
+package com.dicon.shujujiegou.QuickSort;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+/**
+ * name：快速排序
+ * author：dicon
+ * date：2021-7-7
+ * 快速排序在平均状况下要o(nlogn)次比较，通常比其他o(nlogn)算法更快。
+ */
+
+public class quicksort {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        String[] s = scanner.nextLine().split(" ");
+        int[] arr = Arrays.asList(s).stream().mapToInt(Integer::parseInt).toArray();
+        Solution solution = new Solution();
+        solution.sort(arr,0,arr.length-1);
+        System.out.println(Arrays.toString(arr));
+    }
+}
+
+class Solution{
+    public void sort(int[] arr,int left,int right){
+
+        if (left<right){
+
+            int partitionIndex = partiton(arr,left,right);
+            sort(arr,left,partitionIndex -1 );
+            sort(arr,partitionIndex+1,right);
+        }
+
+    }
+
+    public int partiton(int[] arr,int left,int right){
+
+        //设置基准数为left
+        int pivot = left;
+        int index = pivot + 1;
+        for (int i = index;i <= right;i++){
+            if (arr[i]<arr[pivot]){
+                int temp = arr[i];
+                arr[i] = arr[index];
+                arr[index] = temp;
+                index++;
+            }
+        }
+        int temp = arr[pivot];
+        arr[pivot] = arr[index-1];
+        arr[index-1] = temp;
+        return index - 1;
+    }
+}
---
+++ com/dicon/shujujiegou/mergesort/MergeSort.java
@@ -0,0 +1,75 @@
+package com.dicon.shujujiegou.mergesort;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * name：归并排序
+ * author：dicon
+ * date：2021-7-7
+ * 归并排序是分治法的一个应用，时间复杂度为o(nlogn)空间复杂度为o(n)
+ */
+
+public class MergeSort {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        String[] lines = input.nextLine().split(" ");
+        int[] numbs = Arrays.asList(lines).stream().mapToInt(Integer::parseInt).toArray();
+        Solution solution = new Solution();
+        solution.sort(numbs);
+        System.out.println(Arrays.toString(numbs));
+    }
+}
+
+class Solution{
+    public void sort(int[] arr){
+
+        //在排序前，先建好一个长度等于原数组长度的临时数组，避免递归中频繁开辟空间。
+        int[] temp = new int[arr.length];
+        sort(arr,0,arr.length-1,temp);
+    }
+
+    public void sort(int[] arr, int left,int right,int[] temp){
+        if (left<right){
+            int mid = (left+right)/2;
+
+            //左边归并排序，使得左子序列有序
+            sort(arr,left,mid,temp);
+
+            //右边归并排序，使得右子序列有序
+            sort(arr,mid+1,right,temp);
+
+            //将两个有序子数组合并操作
+            merge(arr,left,mid,right,temp);
+        }
+    }
+    public void merge(int[] arr, int left, int mid,int right,int[] temp){
+
+        //左序列指针
+        int i = left;
+
+        //右序列指针
+        int j = mid+1;
+
+        //临时数组指针
+        int t= 0;
+
+        while(i <= mid && j <= right){
+            if (arr[i] <= arr[j]){
+                temp[t++] = arr[i++];
+            }else{
+                temp[t++] = arr[j++];
+            }
+        }
+        while(i <= mid){
+            temp[t++] = arr[i++];
+        }
+        while(j <= right){
+            temp[t++] = arr[j++];
+        }
+        t = 0;
+        while(left<=right){
+            arr[left++] = temp[t++];
+        }
+    }
+}
